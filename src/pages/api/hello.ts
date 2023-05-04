1478b583f6d83036f34c3033fda915855f181209
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-05-04 15:53:40 -0300

    feat: add db

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../prisma/db";
+import { User } from "@prisma/client";
 
 type Data = {
-  name: string
-}
+	users: User[];
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+	const users = await prisma.user.findMany({
+		include: {
+			posts: true,
+		},
+	});
+
+	res.status(200).json({ users });
 }
